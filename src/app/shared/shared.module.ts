06f3123e379e50f82e4e9a0ad9dc4191877af44c
Damion 'Coder' Wilson <126527585+dwilsonstudios@users.noreply.github.com>
2023-10-04 17:19:45 -0400

    Add files via upload

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
+// import { ModalService } from '../services/modal.service';
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  exports: [
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent,
+    InputComponent,
+  ],
+  providers: [provideEnvironmentNgxMask()],
+  //  providers: [ModalService],
+})
+export class SharedModule {}
